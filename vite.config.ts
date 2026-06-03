d80cc93a911a5d1a89e859af9fd5c4011ce49d08
aleckayy <74304315+aleckayy@users.noreply.github.com>
2026-06-03 21:24:17 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,9 @@
 import { defineConfig } from "vite";
-import { lovableTanstackStart } from "@lovable.dev/vite-tanstack-config";
+import viteTanstackConfig from "@lovable.dev/vite-tanstack-config";
 
 export default defineConfig({
   plugins: [
-    lovableTanstackStart({
+    viteTanstackConfig({
       nitro: true,
     }),
   ],
